9e8479026d70ce5c354614ea611f956c3bbbcfd5
vanerb <71894451+vanerb@users.noreply.github.com>
2023-09-20 20:49:27 +0200

    boostrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,10 @@
     <div class="bg-black p-10">
         <img src="img/logocabecera.png" alt="" class="mx-auto h-96 w-96 shadow-lg rounded-lg">
     </div>
-    
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </header>
 
 <div class="mb-20">
